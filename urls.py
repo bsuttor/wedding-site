9b27519f313d0d2b295b7f6d009b039ce562f7dd
Benoît Suttor <benoit.suttor@imio.be>
2015-02-28 19:15:58 +0100

    do not use redirection language

--- urls.py
+++ urls.py
@@ -13,12 +13,7 @@ admin.autodiscover()
 # You can also change the ``home`` view to add your own functionality
 # to the project's homepage.
 
-urlpatterns = i18n_patterns("",
-    # Change the admin prefix here to use an alternate URL for the
-    # admin interface, which would be marginally more secure.
-)
-
-urlpatterns += patterns('',
+urlpatterns = patterns('',
 
     # Cartridge URLs.
     ("^shop/", include("cartridge.shop.urls")),
