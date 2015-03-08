36aa15c82a078b069c78c4e045964dd64ad8d8e7
Benoît Suttor <benoit.suttor@imio.be>
2015-03-08 20:06:05 +0100

    move message into cartridge

--- shop/models.py
+++ shop/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class ShopOrder(Order):
-    billing_detail_message = models.TextField(_("Votre message"), blank=True)
+    #billing_detail_message = models.TextField(_("Votre message"), blank=True)
     discount_total = None
     shipping_total = None
     tax_total = None
